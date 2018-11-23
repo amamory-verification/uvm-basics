e9970150e4a5e066d2164c9c203f4acf7eb8c0c9
Alexandre Amory <amamory@gmail.com>
2018-11-23 09:42:52 -0200

    placing prints to debug test1

--- my_agent.sv
+++ my_agent.sv
@@ -13,14 +13,18 @@ endfunction: new
 
 function void build_phase(uvm_phase phase);
   aport = new("aport", this); 
+  `uvm_info("msg", "Building Agent", UVM_NONE)
   my_sequencer_h = my_sequencer::type_id::create("my_sequencer", this);
   my_driver_h = my_driver::type_id::create("my_driver", this);
   my_monitor_h = my_monitor::type_id::create("my_monitor", this);
+  `uvm_info("msg", "Building Agent DONE!", UVM_NONE)
 endfunction: build_phase
 
 function void connect_phase(uvm_phase phase);
+  `uvm_info("msg", "Connecting Agent", UVM_NONE)
   my_driver_h.seq_item_port.connect(my_sequencer_h.seq_item_export);
   my_monitor_h.aport.connect(aport);
+  `uvm_info("msg", "Connecting Agent DONE!", UVM_NONE)
 endfunction: connect_phase
 
 endclass: my_agent
--- test1.sv
+++ test1.sv
@@ -8,10 +8,13 @@ function new (string name, uvm_component parent);
 endfunction : new
 
 task run_phase(uvm_phase phase);
+  `uvm_info("msg", "Running TEST1", UVM_NONE)
   read_modify_write seq;
   seq = read_modify_write::type_id::create("seq");
   phase.raise_objection(this);
+  `uvm_info("msg", "Starting  SEQ", UVM_NONE)
   seq.start(my_env_h.my_agent_h.my_sequencer_h);
+  `uvm_info("msg", "SEQ STARTED!!!", UVM_NONE)
   phase.drop_objection(this);
 endtask
 
