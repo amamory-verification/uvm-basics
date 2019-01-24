7fcbcafe5ad588fc60a5a8b97b8a1f7796385b03
Alexandre Amory <amamory@gmail.com>
2019-01-24 14:14:28 -0200

    including libcells to the sim file list

--- noc-router/hermes_router/syn/hdl_v.f
+++ noc-router/hermes_router/syn/hdl_v.f
@@ -1,2 +1,4 @@
+/soft64/design-kits/stm/65nm-cmos065_536/CORE65GPSVT_5.1/behaviour/verilog/CORE65GPSVT.v
+/soft64/design-kits/stm/65nm-cmos065_536/CLOCK65GPSVT_3.1/behaviour/verilog/CLOCK65GPSVT.v
 src/layout/RouterCC.v
 src/RouterCC_wrapper.v
